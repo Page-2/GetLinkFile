e50eb8b0f5e36f1181f04d50b188d8935be4e72e
mojitaktir <mojitaktir@gmail.com>
2020-09-03 10:42:18 +0430

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ bot('editmessagetext',[
 ]]),
 ]);
 }elseif($step=="start"){
-if(strpos($text1,"http://soft98.ir/")!==false || strpos($text1,"https://soft98.ir/")!==false ){
+if(strpos($text1,"http://dl2.soft98.ir/")!==false || strpos($text1,"https://dl2.soft98.ir/")!==false ){
 $one=str_replace("https://","",$text1);
 $two=str_replace("http://","",$one);
 $five="https://codeload.$two/zip/master";
