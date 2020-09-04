d3688eacbfda30d2e03d2dd7dc2ccb5b000dc3c4
mojitaktir <mojitaktir@gmail.com>
2020-09-04 18:10:01 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php 
 
 ob_start();
@@ -62,7 +61,7 @@ sendaction($chat_id, typing);
       $file = $video->file_id;
       
       
-      $sizemb = getFilesize(file);
+      $sizemb = getFilesize('$file');
     bot('sendmessage', [
                 'chat_id' => $chat_id,
                 'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$sizemb",
