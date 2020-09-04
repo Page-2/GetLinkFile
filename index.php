4069febef93e5dc6e0363cd03c80e2262907bf32
mojitaktir <mojitaktir@gmail.com>
2020-09-04 19:59:43 +0430

    Update index.php

--- index.php
+++ index.php
@@ -57,9 +57,9 @@ sendaction($chat_id, typing);
         }
 
 
- elseif(isset($message->video)){
- $video = $message->video;
-$file = $video->file_id;
+ elseif(isset($message->document)){
+ $document = $message->document;
+$file = $document->file_id;
       $get = bot('getfile',['file_id'=>$file]);
       $patch = $get->result->file_path;
        $siz = $get->result->file_size;
