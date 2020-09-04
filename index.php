c8cabc56ba211852ff0965a66b3398f133e2cd2a
mojitaktir <mojitaktir@gmail.com>
2020-09-04 12:32:07 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,122 @@
 
+<?php 
 
-<?php
-ini_set('error_reporting', 'E_ALL');
- 
-$botToken = "1246606857:AAHpch56R2GUj_b6RJOYk2UB2MeP3chH9gE";
-$webSite = "https://api.telegram.org/bot" . $botToken;
- 
-$update = file_get_contents("php://input");
-$update = json_decode($update, TRUE);
- 
-$chatId = $update["message"]["chat"]["id"];
-$message = $update["message"]["text"];
- 
-//CustomKeyBord
-$option = array(array("salam", "Key1"), array("key2", "key3"));
-$replyMarkup = array(
- 'keyboard' => $option,
- 'one_time_keyboard' => false,
- 'resize_keyboard' => true,
- 'selective' => true
-);
-$encodedMarkup = json_encode($replyMarkup, true);
- 
-function sendMessage($chatId, $message, $r)
-{
- $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&reply_markup=" . $r;
- file_get_contents($url);
+ob_start();
+
+$API_KEY = '1246606857:AAHpch56R2GUj_b6RJOYk2UB2MeP3chH9gE';
+##------------------------------##
+define('API_KEY',$API_KEY);
+function bot($method,$datas=[]){
+    $url = "https://api.pwrtelegram.xyz/bot".API_KEY."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
 }
- 
-switch ($message) {
- 
- case "/start":
- sendMessage($chatId, "شروع می کنیم", $encodedMarkup);
- break;
- case "salam":
- sendMessage($chatId, "salam be ruye mahet", $encodedMarkup);
- break;
- default:
- sendMessage($chatId, "chi migi ??", $encodedMarkup);
- 
+ function sendmessage($chat_id, $text, $model){
+ bot('sendMessage',[
+ 'chat_id'=>$chat_id,
+ 'text'=>$text,
+ 'parse_mode'=>$mode
+ ]);
+ }
+ function senddocument($chat_id,$document,$caption){
+    bot('senddocument',[
+        'chat_id'=>$chat_id,
+        'document'=>$document,
+        'caption'=>$caption
+    ]);
 }
+ function sendaction($chat_id, $action){
+ bot('sendchataction',[
+ 'chat_id'=>$chat_id,
+ 'action'=>$action
+ ]);
+ }
+ //====================ᵗᶦᵏᵃᵖᵖ======================//
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$from_id = $message->from->id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+//====================ᵗᶦᵏᵃᵖᵖ======================//
+if(preg_match('/^\/([Ss]tart)/',$text)){
+sendaction($chat_id, typing);
+        bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"به ربات تبدیل فایل به لینک خوش امدید",
+            ]);
+        }
+elseif(isset($message->photo)){
+$photo = $message->photo;
+$file = $photo[count($photo)-1]->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+   bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
+ elseif(isset($message->sticker)){
+$sticker = $message->sticker;
+$file = $sticker->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+    bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
+ elseif(isset($message->voice)){
+$voice = $message->voice;
+$file = $voice->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+    bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
+ elseif(isset($message->audio)){
+$audio = $message->audio;
+$file = $audio->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+    bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
+ elseif(isset($message->video)){
+$video = $message->video;
+$file = $video->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+    bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
+ elseif(isset($message->document)){
+$document = $message->document;
+$file = $document->file_id;
+      $get = bot('getfile',['file_id'=>$file]);
+      $patch = $get->result->file_path;
+       $siz = $get->result->file_size;
+    bot('sendmessage', [
+                'chat_id' => $chat_id,
+                'text' =>"https://storage.pwrtelegram.xyz/$patch\n حجم فایلتون :$siz",
+            ]);
+        }
 ?>
