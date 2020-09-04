13bf5ffbdd519a780d6eda5c3c26529d326d2625
mojitaktir <mojitaktir@gmail.com>
2020-09-04 12:19:58 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 
-<?php ini_set('error_reporting', 'E_ALL');
-$botToken = "1246606857:AAHpch56R2GUj_b6RJOYk2UB2MeP3chH9gE"; 
+
+<?php
+ini_set('error_reporting', 'E_ALL');
+ 
+$botToken = "1246606857:AAHpch56R2GUj_b6RJOYk2UB2MeP3chH9gE";
 $webSite = "https://api.telegram.org/bot" . $botToken;
-$update = file_get_contents("php://input"); 
-$update = json_decode($update, TRUE); 
-$chatId = $update["message"]["chat"]["id"]; $message = $update["message"]["text"]; 
-switch ($message) { case "/start": sendMessage($chatId, "شروع می کنیم");
-break;
-case "salam": sendMessage($chatId, "salam be ruye mahet");
-break;
-default: sendMessage($chatId, "chi migi ??");
+ 
+$update = file_get_contents("php://input");
+$update = json_decode($update, TRUE);
+ 
+$chatId = $update["message"]["chat"]["id"];
+$message = $update["message"]["text"];
+ 
+//CustomKeyBord
+$option = array(array("salam", "Key1"), array("key2", "key3"));
+$replyMarkup = array(
+ 'keyboard' => $option,
+ 'one_time_keyboard' => false,
+ 'resize_keyboard' => true,
+ 'selective' => true
+);
+$encodedMarkup = json_encode($replyMarkup, true);
+ 
+function sendMessage($chatId, $message, $r)
+{
+ $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&reply_markup=" . $r;
+ file_get_contents($url);
 }
-function sendMessage($chatId, $message) 
-{ $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message);
-file_get_contents($url); 
-} ?>
+ 
+switch ($message) {
+ 
+ case "/start":
+ sendMessage($chatId, "شروع می کنیم", $encodedMarkup);
+ break;
+ case "salam":
+ sendMessage($chatId, "salam be ruye mahet", $encodedMarkup);
+ break;
+ default:
+ sendMessage($chatId, "chi migi ??", $encodedMarkup);
+ 
+}
+?>
